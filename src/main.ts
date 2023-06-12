ad13d139d2cdfb272f89c2a25b2eced19b6c9363
dtg-lucifer <piushbasu@gmail.com>
2023-06-12 19:50:13 +0530

    Minor bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 const para = document.querySelectorAll(".T");
 const headingOne = document.querySelector("#H_ONE");
+const skipBtn = document.querySelector("#skip__btn");
+const loginSection = document.querySelector("#login");
 
 if (para.length > 0) {
     const paraOneObserver = new IntersectionObserver((entries) => {
@@ -29,4 +31,23 @@ if (headingOne) {
     });
 
     headingOneObserver.observe(headingOne);
+}
+
+if (skipBtn && loginSection) {
+    const loginSectionObserver = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    skipBtn.classList.add("hide");
+                } else {
+                    skipBtn.classList.remove("hide");
+                }
+            });
+        },
+        {
+            threshold: 0.5,
+        }
+    );
+
+    loginSectionObserver.observe(loginSection);
 }
